d8ad4ca11fb3c5a62a2d94fe02e7c36057522ac3
beloveugenii <beloveugenii@gmail.com>
2022-12-22 16:37:36 +0300

    changes in termux functions: now it`s a closures;

--- simple-sport.pl
+++ simple-sport.pl
@@ -8,34 +8,33 @@ use utf8;
 use open qw / :std :utf8 /;
 use Time::HiRes qw / time sleep /;
 use Getopt::Long qw / GetOptions /;
+use subs qw / prepare_termux /;
 
 require "./libsport.pm";
-require "./t_sound.pm";
 
-
-our $version = '0.0.2';
+my $version = '0.0.2';
 
 # Создаем объект тренировок
 my $training = Training->new;
 
 # Обработчики для внешних опций
 sub handler { $training->set_option( @_ ) }
-sub sound { 
-    # Если мы в Termux и звук включен
-    if ( $ENV{HOME} =~ /\/data.+/ ) {
-        # Создаем объект и включаем звук
-        my $termux = T_sound->new;
-        $termux->enable;
-        # Устанавливаем обработчик прерывания
-        $SIG{INT} = \$termux->restore
-    }
+
+my ( $sound_on, $sound_off );
+
+# Если мы находимся в среде Termux
+if ( $ENV{HOME} =~ /\/data.+/ ) {
+    ( $sound_on, $sound_off ) = prepare_termux;
+    
+    # Устанавливаем обработчик прерывания
+    $SIG{INT} = $sound_off
 }
 
 GetOptions (
     'repeats=i' => \&handler,
     'pause=s' => \&handler,
     'relax=s' => \&handler,
-    'sound' => \&sound,
+    'sound' => $sound_on,
     'v|version' => sub { print "$version\n" },
 );
 
@@ -43,6 +42,40 @@ GetOptions (
 $training->add(grep -e $_, @ARGV);
 $training->prepare;
 $training->show_exercises;
+<STDIN>;
+
+&$sound_off;
+
+
+
+
+# Функция возвращает две анонимные фукнции
+# Одна включает в Termux звук
+# Другая возращает среду в исходное состояние
+sub prepare_termux {
+    my $termux = "$ENV{HOME}/.termux/termux.properties";
+    my $backup = $termux . '.bak';
+    return 
+        sub {
+            rename $termux, $backup or die "$!";
+            open my $new_file, '>', $termux;
+            open my $old_file, '<', $backup;
+            # Читаем файл резервной копии и одновременно записываем данные оттуда в новый файл. 
+            # Когда будет найдена строка с 'beep' она будет изменена и записана в новом виде
+            while ( <$old_file> ) {
+                s/#(.+\s)(beep)/$1$2/;
+                print $new_file $_
+            }
+            # Перезагружаем настройки Termux и подаем звуковой сигнал
+            system 'termux-reload-settings';
+            print "\a"
+        },
+        sub {
+            rename $backup, $termux if -e $backup;
+            system 'termux-reload-settings';
+            exit 0
+        }
+}
 
 
 # POD
