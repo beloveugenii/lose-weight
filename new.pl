da7a57d8e68807fe465b9a3f59929e2a5a4225c4
Евгений Белов <eugeniibelov@gmail.com>
2023-04-07 13:00:52 +0300

    work under new ver continuing

--- new.pl
+++ new.pl
@@ -2,84 +2,41 @@
 
 use strict;
 use FindBin qw / $Bin /;
+use Time::HiRes qw / time sleep /;
 use feature 'say';
 use utf8;
 use open qw / :std :utf8 /;
 use lib "$Bin";
 require "$Bin/../screen.pm";
 require "$Bin/libss.pm";
-my $screen = Screen->new;
-
-my @reserved_words = qw / name repeats pause relax on_end aref /;
-
-
-
-sub parse_files {
-
-    my $files_ref = shift;
-    my @parsed_files;
-
-    foreach my $file ( @$files_ref ) {
-
-        open my $fh, "<", $file or die "'$file': $!";
-        my %file;
-        #%file = (
-            #param1 => str, 
-            #param2 => str, 
-            #paramN => str, 
-            #aref=> [
-                #[ ex1, dur1, ], 
-                #[ ex2, dur2, ], 
-                #[ exN, durN ],
-            #],
-        #);
-    
-        # Parser
-        while ( my $line = <$fh> ) {
-            chomp $line;
-            next if $line =~ /(?:^#+)|(?:^$)/;
-
-            $line =~ /^(.+)(?::|(?:->))(.+)$/;
-
-            ( grep { $1 eq $_} @reserved_words ) ? 
-            ###!!!!!!!!!!!!1
-            #
-            #    КАК ОТЛИЧАЕТСЯ СТРОКА С ПАРАМЕТРОМ И СТРОКА С УПРАЖНЕНИЕМ?????
-            #
-            ##
-                    $file{$1} = to_sec($2) :
-                    push @{$file{aref}}, [ $1, to_sec($2) ];
-            
-        }
-        push @parsed_files, \%file
-    }
-
-       
-    \@parsed_files
-    #@parsed_files = (
-        #href1,
-        #href2,
-    #);
-}
-
-
-my $str = sub {
-    my $out_str;
-    foreach my $href ( @{$_[0]} ) {
-        my $name = ( $href->{name} =~ /^['"](.+)['"]$/ ) ? $1 : $href->{name};
-        my $duration;
-        $duration += $_->[1] foreach @{$href->{aref}};
-        $out_str .= "$name: " . int ( $duration / 60 ) . 'm ' . $duration % 60 . "s\n";
-    }
-    $out_str
-};
 
 my $aref = parse_files (check_list( \@ARGV )) ;
 
 
-for my $key ( keys %{$aref->[0]} ) {
+foreach my $training ( @$aref ) {
+    # Для каждого элемента из массива с файлами упражнений
+    my ($pause, $relax, $repeats, $name) = ( $training->{pause}, $training->{relax}, $training->{repeats}, $training->{name});
 
-say "$key = $aref->[0]{$key}";
+    # Для каждого повтора
+    for ( my $repeat_number = 1; $repeat_number <= $repeats; $repeat_number++ ) {
+
+        Screen->header("$name: $repeat_number / $repeats");
+
+        for ( my $ex_number = 0; $ex_number <= $#{$training->{aref}}; $ex_number++ ) {
+
+            #### ТЕПЕРЬ КАЖДУЮ СТРОКУ НУЖНО РАЗБИТЬ НА НАЗВАНИЕ И ВРЕМЯ ВЫПОЛНЕНИЯ
+
+
+            say "Приготовься $pause" if $ex_number == 0 && $repeat_number == 1;
+            say "@{$training->{aref}->[$ex_number]}";
+            say "Пауза $pause" unless $ex_number == $#{$training->{aref}};
+            say "Время отдохнуть $relax" if $ex_number == $#{$training->{aref}} && $repeat_number != $repeats;
+        
+        }
+
+        say "Конец тренировки" if $repeat_number == $repeats;
+        say
+    }
 
 }
 
