5f8a7c422afb227ab81d85d9cc27405996414041
Belov Eugeniy <beloveugenii@gmail.com>
2023-09-06 03:12:38 +0300

    555

--- diary/user.py
+++ diary/user.py
@@ -1,68 +1,15 @@
 #!/usr/bin/env python3
 
-import sqlite3
+import os
+import libfc as fc
+#try:
+#    f = open(".config")
+#except FileNotFoundError:
 
-def check_and_return(text, that):
-    # Эта функция получает строку и параметр, куда предполагается ее сохранить.
-    # Возвращает приведенный в нужную форму параметр или зацикливает ввод до тех пор, пока не будет введено нужное значкние.
-    if that == 'имя':
-        while True:
-            if text.isalpha() and len(text) > 1:
-                return text[0].upper() + text[1:]
-            else:
-                text = input('Здесь требуется строка: ' )
-    elif that == 'пол':
-        while True:
-            if text in 'мМжЖ':
-                return text.lower()
-            else:
-                text = input('Введите \'м\' или \'ж\': ')
-    else:
-        while True:
-            if text.isnumeric():
-                return float(text)
-            else:
-                text = input('Здесь требуется число: ')
+ud = fc.get_data('user_data', 0)
+
+#    для мужчин: (10 * ud['weight'] + 6.25 * ud['height'] – 5 * ud['age'] + 5) * ud['activity'];
+
+# для женщин: (10 * ud['weight'] + 6.25 * ud['height'] – 5 * ud['age'] - 161) * ud['activity'];
 
 
-        
-        
-
-#cur.execute('CREATE TABLE users (name TEXT, age INT, sex TEXT, height INT, weight INT, activity REAL);')
-#запуск, проверка есть ли пользователь.
-    # проверка есть ли файл конфиг
-f = ''
-while not f:
-    try:
-        f = open('.config', 'r')
-        ## читаем файл и получаем id, по которому нвходим в бд всю инфу
-        for line in f:
-            print(line)
-        f.close()
-    
-    except FileNotFoundError:
-        print("Похоже, что это первый запуск.\nНеобходимо ввести некоторые данные о себе.")
-
-## !!!!!!? сд4лать здесььчловарь
-        p = ('имя', 'возраст', 'пол', 'вес', 'рост', 'активность')
-
-        v = []
-
-        for i in range(len(p)):
-            v.append(check_and_return(input('Введите ' + p[i] + ': ' ), p[i]))
-        
-
-        #cur.execute('INSERT INTO users VALUES(?, ?, ?, ?, ?)', v)
-        #con.commit()
-
-# надо записать rowid польщователя в файл 
-
-        f = open('.config', 'w')
-        for d in v:
-            f.write(str(d) + '\n')
-        f.close()
-    
-
-
-
-#записи в дневнике с id пользователя
